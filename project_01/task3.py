0cbb263cbadfa6a488195f1315f2aec1a9fc3edf
Shinho Kang <shinho.kang@honestly.de>
2017-05-25 22:44:29 +0200

    rename: task2 -> task3, use matplotlib.pyplot to show an image

--- project_01/task3.py
+++ project_01/task3.py
@@ -1,11 +1,13 @@
 ################################################################################
-# Task 1.2
+# Task 1.3
 # Illumination Compensation
 ################################################################################
 
 import numpy as np
 import scipy.misc as misc
 from auxiliar import load_images
+import matplotlib.pyplot as plt
+
 
 # load the images in the global scope
 images = load_images()
@@ -14,7 +16,8 @@ images = load_images()
 def illumination():
     """Implement ..."""
     bauckhage = images['bauckhage']
-    misc.imshow(bauckhage)
-
+    # misc.imshow(bauckhage)
+    plt.imshow(bauckhage)
+    plt.show()
 
 illumination()
