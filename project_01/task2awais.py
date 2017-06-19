28852b5318d3801c43aac0325b85ef596b3b0f44
Omar Gutiérrez <omar.vpa@gmail.com>
2017-06-19 10:37:54 +0200

    Subtle modifications to the implementation by Awais

--- project_01/task2awais.py
+++ project_01/task2awais.py
@@ -16,36 +16,24 @@ def writeImage(data,filename):
     misc.toimage(data,cmin=0,cmax=255).save(filename)
 
 
-#LloydMax Algorithm
-def LloydAlgorithm(imgData):
+def LloydAlgorithm(imgData, levels=2):
+    """Implement the Lloyd Max quantization algorithm"""
     totalIterations = 100
     threshold = 0.01
 
     #first calculate the intensity histogram
     histogram = np.histogram(imgData, bins=np.arange(257))[0]
 
-    #calculate PDF
-    totalSum = histogram.sum()
-    probs = np.ndarray(shape=(256,))
-    for i in range(len(histogram)):
-        probs[i] = histogram[i]/float(totalSum)
+    # calculate PDF
+    probs = histogram/histogram.sum()
 
-    #choose number L of quantization levels
-    level = 256
-
-    #initialize the boundaries
-    boundaries = np.ndarray(shape=(level+1,))
-    for i in range(level+1):
-        boundaries[i] = float(i)*256.0/float(level)
-
-    #initialize the quantization points bv
-    pointsBv = np.ndarray(shape=(level,))
-    for i in range(level):
-        pointsBv[i] = float(i)*(256.0/float(level))+(256.0/2.0*float(level))
+    #initialize the boundaries and quantization points
+    boundaries = np.arange(levels+1) * 256/levels
+    pointsBv = (np.arange(levels) * 256/levels) + 256/(2*levels)
 
     #iterate the equations
     error = 0.0
-    for i in xrange(level):
+    for i in xrange(levels):
         currentBoundary = boundaries[i]
         nextBoundary = boundaries[i+1]
         for j in xrange(int(currentBoundary),int(nextBoundary)):
@@ -55,11 +43,11 @@ def LloydAlgorithm(imgData):
     while(iterationCount<totalIterations and error>threshold):
         iterationCount+=1
         #update boundaries
-        for i in range(level):
+        for i in range(levels):
             boundaries[i] = (pointsBv[i]+pointsBv[i-1])/2.0
 
         #update points
-        for i in range(level):
+        for i in range(levels):
             numerator = 0.0
             denominator = 0.0
             currentBoundary = boundaries[i]
@@ -76,18 +64,20 @@ def LloydAlgorithm(imgData):
                 pointsBv[i] = float(numerator)/float(denominator)
 
 
-    rows,cols = imgData.shape
-    newImgData = np.ndarray(shape=(256,256))
+    rows, cols = imgData.shape
+    newImgData = np.ndarray(shape=(rows, cols))
     for i in xrange(rows):
         for j in xrange(cols):
-            for k in range(level):
+            for k in range(levels):
                 if(imgData[i,j]>=boundaries[k] and imgData[i,j]<boundaries[k+1]):
                     break
             newImgData[i,j] = pointsBv[k]
 
-    writeImage(newImgData,'result.png')
+    return newImgData
 
 
 
-
-LloydAlgorithm(readImage('../../images/bauckhage-gamma-2.png'))
+image = readImage('images/bauckhage-gamma-2.png')
+result = LloydAlgorithm(image, levels=4)
+misc.imshow(result)
+writeImage(result,'result.png')
