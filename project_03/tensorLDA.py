6339fb9589b8029cca5a9a8e6672b254227a89bc
shinhokang <wis.shinho.kang@gmail.com>
2017-07-20 05:02:54 +0200

    add timer on task2

--- project_03/tensorLDA.py
+++ project_03/tensorLDA.py
@@ -1,4 +1,3 @@
-
 import tarfile
 import numpy as np
 import os
@@ -7,11 +6,13 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import random
+import timeit
 
 IMG_SIZE = 2511
 label_dict = {0: 'Background', 1: 'Car'}
 TEST_DIR = 'uiuc/test'
 TRAIN_DIR = 'uiuc/train'
+
 
 def draw(vector, shape):
     plt.figure()
@@ -22,28 +23,32 @@ def draw(vector, shape):
 
 def readData(dirName):
     for root, _, files in os.walk(dirName):
-        data = np.zeros(shape=(len(files),81, 31))
+        data = np.zeros(shape=(len(files), 81, 31))
         counter = 0
         for filename in files:
             if (filename.find("DS_Store") == 1):
                 continue
             path = os.path.join(root, filename)
             image = misc.imread(path, flatten=True)
-            image = misc.imresize(image,size=(81,31)).astype("float64")
+            image = misc.imresize(image, size=(81, 31)).astype("float64")
             data[counter] = preprocessing.normalize(image)
-            counter+=1
+            counter += 1
     label = np.char.array(files).rfind('Pos').astype(np.float32)
-    posClass = abs(1/float(len(files)))
-    negClass = abs(1/float(len(files)))*-1
+    posClass = abs(1 / float(len(files)))
+    negClass = abs(1 / float(len(files))) * -1
     label[np.where(label > 0)] = posClass
-    label[np.where(label == -1)] =  negClass
-    return data,label
+    label[np.where(label == -1)] = negClass
+    return data, label
 
-def saveImage(imgData,filename):
-    misc.imsave(filename,imgData)
+
+def saveImage(imgData, filename):
+    misc.imsave(filename, imgData)
+
 
 def readTrainingData():
     return readData(TRAIN_DIR)
+
+
 def readTestData():
     return readData(TEST_DIR)
 
@@ -71,28 +76,27 @@ def Gram_Schmidt(vecs, row_wise_storage=True, tol=1E-10):
     return np.transpose(V) if row_wise_storage else V
 
 
-
-def evaluateClassifier(classifier,data,W,labels):
-    tp=0
-    fp=0
-    tn=0
-    fn=0
+def evaluateClassifier(classifier, data, W, labels):
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
     projectedLabels = []
 
-    for item in data[labels==labels.max()]: #check for pos
-        if np.dot(W.ravel().T,item.ravel()) >= classifier:
+    for item in data[labels == labels.max()]:  # check for pos
+        if np.dot(W.ravel().T, item.ravel()) >= classifier:
             prediction = 1
         else:
             prediction = 0
 
-        if(prediction!=1):
-            fp+=1
+        if (prediction != 1):
+            fp += 1
         else:
-            tp+=1
+            tp += 1
         projectedLabels.append(prediction)
 
-    for item in data[labels==labels.min()]: #check for neg
-        if np.dot(W.ravel().T,item.ravel()) >= classifier:
+    for item in data[labels == labels.min()]:  # check for neg
+        if np.dot(W.ravel().T, item.ravel()) >= classifier:
             prediction = 1
         else:
             prediction = 0
@@ -103,15 +107,10 @@ def evaluateClassifier(classifier,data,W,labels):
             tn += 1
         projectedLabels.append(prediction)
 
-    precision = tp/float(tp+fp) if (tp+fp)>0 else 0
-    recall = tp/float(tp+fn) if (tp+fn)>0 else 0
+    precision = tp / float(tp + fp) if (tp + fp) > 0 else 0
+    recall = tp / float(tp + fn) if (tp + fn) > 0 else 0
 
-    return (tp+tn)/float(tp+tn+fp+fn),precision,recall,projectedLabels
-
-
-
-
-
+    return (tp + tn) / float(tp + tn + fp + fn), precision, recall, projectedLabels
 
 
 if __name__ == '__main__':
@@ -119,8 +118,8 @@ if __name__ == '__main__':
 
     # calculate means for two classes
     means = []
-    means.append(np.mean(dataset[labels==labels.min()],axis=0));
-    means.append(np.mean(dataset[labels==labels.max()],axis=0));
+    means.append(np.mean(dataset[labels == labels.min()], axis=0));
+    means.append(np.mean(dataset[labels == labels.max()], axis=0));
 
     # calculate overall mean
     overall_mean = np.mean(dataset, axis=0)
@@ -128,6 +127,7 @@ if __name__ == '__main__':
     p = 9
     us = np.zeros(shape=(p, dataset.shape[1]))
     vs = np.zeros(shape=(p, dataset.shape[2]))
+    start = timeit.default_timer()
     for r in range(p):
 
         u = np.random.rand(dataset.shape[1])
@@ -149,7 +149,7 @@ if __name__ == '__main__':
             t_st_v = np.vstack((v, v_temp))
             v_gs = Gram_Schmidt(t_st_v)
             v_temp = v_gs[-1]
-            v = preprocessing.normalize(v_temp.reshape(1,-1))[0]
+            v = preprocessing.normalize(v_temp.reshape(1, -1))[0]
 
             # compute v contraction
             contractionV = np.empty((dataset.shape[0], 81))
@@ -162,7 +162,7 @@ if __name__ == '__main__':
             t_st_u = np.vstack((u, u_temp))
             u_gs = Gram_Schmidt(t_st_u)
             u_temp = u_gs[-1]
-            u = preprocessing.normalize(u_temp.reshape(1,-1))[0]
+            u = preprocessing.normalize(u_temp.reshape(1, -1))[0]
 
             t = t + 1
             check = abs(u[t] - u[t - 1])
@@ -172,18 +172,21 @@ if __name__ == '__main__':
         us[r] = u
         vs[r] = v
 
+    stop = timeit.default_timer()
+    computeContractionDuration = stop - start
+
     W = np.zeros(shape=(dataset.shape[1], dataset.shape[2]))
     for r in range(p):
-        W += np.outer(us[r],vs[r])
-    draw(W,(81,31))
-    #misc.imsave('tensorWOut.jpg',W)
+        W += np.outer(us[r], vs[r])
+    draw(W, (81, 31))
+    # misc.imsave('tensorWOut.jpg',W)
 
 
 
     mu = []
     mu.append(np.dot(W.ravel().T, means[0].ravel()))
     mu.append(np.dot(W.ravel().T, means[1].ravel()))
-    print mu
+    print(mu)
 
     # create k = 1,...,10 different classifier
     cl_total = 100
@@ -193,29 +196,33 @@ if __name__ == '__main__':
         classifiers.append(theta)
     bestThreshold = classifiers[0]
 
-    bestPerformance, precision, recall,projectedLabels = evaluateClassifier(bestThreshold,dataset,W, labels)
+    start = timeit.default_timer()
+
+    bestPerformance, precision, recall, projectedLabels = evaluateClassifier(bestThreshold, dataset, W, labels)
     precisions = np.zeros((len(classifiers),))
     recalls = np.zeros((len(classifiers)))
-    for i,threshold in enumerate(classifiers):
-        print("Threshold = ",threshold)
-        performance,precision,recall,projectedLabels = evaluateClassifier(threshold,dataset,W,labels)
+    for i, threshold in enumerate(classifiers):
+        print("Threshold = ", threshold)
+        performance, precision, recall, projectedLabels = evaluateClassifier(threshold, dataset, W, labels)
         precisions[i] = precision
         recalls[i] = recall
         print("Performance = ", performance)
         print("Precision = ", precision)
         print("Recall = ", recall)
-        if(performance>bestPerformance):
+        if (performance > bestPerformance):
             bestPerformance = performance
             bestThreshold = threshold
 
+    stop = timeit.default_timer()
+    evaluationDuration = stop - start
     print('Best Threshold = ', bestThreshold)
     print("Best Performance = ", bestPerformance)
     print("Projected labels: ")
-    print projectedLabels
-    print "Recalls:"
-    print recalls
-    print "Precisions: "
-    print precisions
+    print(projectedLabels)
+    print("Recalls:")
+    print(recalls)
+    print("Precisions: ")
+    print(precisions)
 
     plt.clf()
     plt.plot(recalls, precisions, lw=2, color='navy',
@@ -229,9 +236,7 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.show()
 
-
-
-    print W
+    print(W)
     # plot W
     plt.plot(W)
     plt.grid()
@@ -242,12 +247,19 @@ if __name__ == '__main__':
     newData, labels = readTestData()
     labels = np.ones((newData.shape[0],))
     projectedLabels = []
+    start = timeit.default_timer()
     for item in newData:  # check for pos
-        if np.dot(W.ravel().T,item.ravel()) >= bestThreshold:
+        if np.dot(W.ravel().T, item.ravel()) >= bestThreshold:
             prediction = 1
         else:
             prediction = 0
         projectedLabels.append(prediction)
     errors = np.sum(projectedLabels != labels)
-    print errors
-    print projectedLabels
+    stop = timeit.default_timer()
+    predictionDuration = stop - start
+    print(errors)
+    print(projectedLabels)
+
+    print("computeContractionDuration", computeContractionDuration)
+    print("evaluationDuration", evaluationDuration)
+    print("predictionDuration", predictionDuration)
